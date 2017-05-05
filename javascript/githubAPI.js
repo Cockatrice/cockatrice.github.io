ac83be40673acc1acf7bce4251475b22e94aba0d
Zach H <Zahalpern+github@gmail.com>
2017-05-05 14:53:49 -0400

    js to work with releases and dev builds

--- javascript/githubAPI.js
+++ javascript/githubAPI.js
@@ -5,13 +5,13 @@ $.getJSON(githubReleasesAPI, function (json) {
     var ubuntu = 'https://github.com/Cockatrice/Cockatrice/releases/latest'
     for (asset of json.assets) {
       url = asset.browser_download_url
-      if (url.includes('win64.exe')) {
+      if (ur.includes('win64.exe') || url.includes('x86_64_qt5.exe')) {
         win64 = url
       }
-      else if (url.includes('osx.dmg')) {
+      else if (url.includes('.dmg')) {
         macOS = url
       }
-      else if (url.includes('trusty.deb')) {
+      else if (url.includes('.deb')) {
         ubuntu = url
       }
     }
