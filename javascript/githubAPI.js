10aa29a3aa7c4e0d043b3f4573ea1a33520f3d51
tooomm <tooomm@users.noreply.github.com>
2025-03-29 13:33:48 +0100

    dynamic news

--- javascript/githubAPI.js
+++ javascript/githubAPI.js
@@ -1,34 +1,35 @@
 var githubReleasesAPI = "https://api.github.com/repos/Cockatrice/Cockatrice/releases/latest";
 
+// Update download links based on the latest release assets
 $.getJSON(githubReleasesAPI, function (json) {
-    var win64 = 'https://github.com/Cockatrice/Cockatrice/releases/latest'
-    var macOS_latest = 'https://github.com/Cockatrice/Cockatrice/releases/latest'
-    var macOS_legacy = 'https://github.com/Cockatrice/Cockatrice/releases/latest'
-    var ubuntu = 'https://github.com/Cockatrice/Cockatrice/releases/latest'
-    var debian = 'https://github.com/Cockatrice/Cockatrice/releases/latest'
-    var fedora = 'https://github.com/Cockatrice/Cockatrice/releases/latest'
-    
+    var win64 = 'https://github.com/Cockatrice/Cockatrice/releases/latest';
+    var macOS_latest = 'https://github.com/Cockatrice/Cockatrice/releases/latest';
+    var macOS_legacy = 'https://github.com/Cockatrice/Cockatrice/releases/latest';
+    var ubuntu = 'https://github.com/Cockatrice/Cockatrice/releases/latest';
+    var debian = 'https://github.com/Cockatrice/Cockatrice/releases/latest';
+    var fedora = 'https://github.com/Cockatrice/Cockatrice/releases/latest';
+
     for (asset of json.assets) {
-      url = asset.browser_download_url
-      console.log(url);
-      if (url.includes('Win10')) {
-        win64 = url
-      }
-      else if (url.includes('macOS15')) {
-        macOS_latest = url
-      }
-      else if (url.includes('macOS14')) {
-        macOS_legacy = url
-      }
-      else if (url.includes('Ubuntu24')) {
-        ubuntu = url
-      }
-      else if (url.includes("Debian")) {
-        debian = url
-      }
-      else if (url.includes("Fedora")) {
-        fedora = url
-      }
+        url = asset.browser_download_url;
+        console.log(url);
+        if (url.includes('Win10')) {
+            win64 = url;
+        }
+        else if (url.includes('macOS15')) {
+            macOS_latest = url;
+        }
+        else if (url.includes('macOS14')) {
+            macOS_legacy = url;
+        }
+        else if (url.includes('Ubuntu24')) {
+            ubuntu = url;
+        }
+        else if (url.includes("Debian")) {
+            debian = url;
+        }
+        else if (url.includes("Fedora")) {
+            fedora = url;
+        }
     }
 
     $('#win64').attr('href', win64);
@@ -37,4 +38,17 @@ $.getJSON(githubReleasesAPI, function (json) {
     $('#ubuntu').attr('href', ubuntu);
     $('#debian').attr('href', debian);
     $('#fedora').attr('href', fedora);
+}).fail(function() {
+    console.error('Failed to fetch download links.');
+});
+
+// Update news section with the latest release information
+$.getJSON(githubReleasesAPI, function(json) {
+    var releaseName = json.name;
+    var releaseDate = new Date(json.published_at).toISOString().split('T')[0];
+    var releaseUrl = json.html_url;
+    var newsItem = `<li><kbd>${releaseDate}</kbd> <a href="${releaseUrl}" target="_blank" rel="noreferrer">${releaseName}</a> released!</li>`;
+    $('#news-list').html(newsItem);    // Replace static content with newest release item fetched
+}).fail(function() {
+    console.error('Failed to fetch release news.');
 });
