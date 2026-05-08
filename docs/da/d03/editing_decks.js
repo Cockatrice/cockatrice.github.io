ac742e08f67a85f6c52b992ea78777b3ddbaf450
ZeldaZach <ZeldaZach@users.noreply.github.com>
2026-05-08 14:05:02 +0000

    deploy: Cockatrice/Cockatrice@0672603755fa9beeb543831a2aadd67fe6bd9a4c

--- docs/da/d03/editing_decks.js
+++ docs/da/d03/editing_decks.js
@@ -3,15 +3,15 @@ var editing_decks =
     [ "Classic Deck Editor", "d3/d32/editing_decks_classic.html", [
       [ "Editing Basic Deck Information", "d3/d32/editing_decks_classic.html#editing-basic-deck-information", null ],
       [ "Adding Cards", "d3/d32/editing_decks_classic.html#adding-cards", null ],
-      [ "Modifying the Deck List", "d3/d32/editing_decks_classic.html#modifying-the-deck-list", null ],
-      [ "Modifying printings", "d3/d32/editing_decks_classic.html#modifying-printings", null ]
+      [ "Modifying the Deck List", "d3/d32/editing_decks_classic.html#modifying-the-deck-list-1", null ],
+      [ "Modifying printings", "d3/d32/editing_decks_classic.html#modifying-printings-1", null ]
     ] ],
     [ "Visual Deck Editor", "de/d6e/editing_decks_visual.html", [
       [ "Editing Basic Deck Information", "de/d6e/editing_decks_visual.html#editing-basic-deck-information-1", null ],
       [ "Adding Cards", "de/d6e/editing_decks_visual.html#adding-cards-1", null ],
-      [ "Modifying the Deck List", "de/d6e/editing_decks_visual.html#modifying-the-deck-list-1", null ],
+      [ "Modifying the Deck List", "de/d6e/editing_decks_visual.html#modifying-the-deck-list", null ],
       [ "Modifying the visual deck layout", "de/d6e/editing_decks_visual.html#modifying-the-visual-deck-layout", null ],
-      [ "Modifying printings", "de/d6e/editing_decks_visual.html#modifying-printings-1", null ],
+      [ "Modifying printings", "de/d6e/editing_decks_visual.html#modifying-printings", null ],
       [ "Deck Analytics", "de/d6e/editing_decks_visual.html#deck-analytics", null ],
       [ "Sample Hand", "de/d6e/editing_decks_visual.html#sample-hand", null ]
     ] ],
